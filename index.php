a293b05a30e30c225dfb51bcc0786ce144fa26d3
Iyanuoluwa Bayo <63076848+zilyn@users.noreply.github.com>
2023-03-04 18:51:34 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>stylish animated login form</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<section>
+
+<div class="leaves">
+	<div class="set">
+		<div><img src="leaf_02.png"></div>
+		<div><img src="leaf_03.png"></div>
+		<div><img src="leaf_04.png"></div>
+		<div><img src="leaf_02.png"></div>
+		<div><img src="leaf_03.png"></div>
+		<div><img src="leaf_04.png"></div>
+		<div><img src="leaf_02.png"></div>
+		<div><img src="leaf_04.png"></div>
+	</div>
+</div>
+
+
+	<img src="1.jpg" class="bg">
+	<img src="girln.png" class="girl">
+
+<div class="login">
+<h2>Sign In</h2>
+<div class="inputBox">
+<input type="text" placeholder="Username">
+</div>
+
+<div class="inputBox">
+<input type="password" placeholder="Password">
+</div>
+<style type="text/css">
+
+.login .inputBox #btn 
+{
+	position: relative;
+	border: none;
+	outline: none;
+	background: #ea682c;
+	color:#fff;
+	cursor: pointer;
+	font-size: 1.25em;
+	font-weight: 500;
+	transition: 0.5s;
+}
+</style>
+
+<div class="inputBox">
+<input type="submit" value="Login" id="btn">
+</div>
+
+<div class="group">
+<a href="#">Forget Password</a>
+<a href="#">Signup</a>
+</div>
+</div>
+
+	
+</section>
+</body>
+</html>
